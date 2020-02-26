2ebe07801628cbdd802272edd444a7b2b45e2f3c
student <student@Admins-iMac.local>
2020-02-26 16:19:46 -0600

    2nd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,21 @@ import Films from './Films';
 import Film from './Film';
 import People from './People';
 import Person from './Person';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Fragment>
-                    <Link to='/'><button>Go Home</button></Link>
-                    <Link to='/Films'><button>View Films</button></Link>
-                    <Link to='/People'><button>View People</button></Link>
+                    <div className='divHeader'>
+                        <img alt='logo' className='imgLogo' src={require('../assets/logo.png')} />
+                        <div className='divButtons'>
+                            <Link to='/'><button>Go Home</button></Link>
+                            <Link to='/Films'><button>View Films</button></Link>
+                            <Link to='/People'><button>View People</button></Link>
+                        </div>
+                    </div>
                     <Switch>
                         <Route exact path='/Films' component={Films} />
                         <Route exact path='/Films/:id' component={Film} />
